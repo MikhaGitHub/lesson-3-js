aecc51277d5a006a8e2a4cc5e5e2f5ef5169a382
MikhaGitHub <https://github.com/MikhaGitHub/Parcing-data-from-football-matches>
2025-02-18 22:22:17 +0300

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+// по сути получаем списки глобально а позже динамически через callback function 
+// получаем значения полученных переменных return string 
+const personalityInput = document.getElementById("personality");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const resultElement = document.querySelector('.result');
+
+// Определяем функцию, которая будет выполняться при нажатии кнопки
 function updateResult() {
-    var personality_value = document.getElementById("personality").value;
-    var month_value = document.getElementById("month").value;
-    var year_value = document.getElementById("year").value;
+    let personality_value = personalityInput.value;
+    let month_value = monthInput.value;
+    let year_value = yearInput.value;
 
-    var resultText = `${personality_value} ${month_value} ${year_value}`;
+    let resultText = `${personality_value} ${month_value} ${year_value}`;
 
-    document.querySelector('.result').innerHTML = resultText;
-};
+    resultElement.innerHTML = resultText;
+}
+
+// Добавляем слушатель события на кнопку
+updateButton.addEventListener('click', updateResult);
